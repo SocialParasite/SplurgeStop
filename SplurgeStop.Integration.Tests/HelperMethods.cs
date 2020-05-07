a671b08ad69fd7631a79e7373195e7b6fcfc5b8e
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-05-07 16:53:39 +0300

    HelperMethods & integration tests
    HelperMethods static class created with two methods. First one creates a valid PurchaseTransaction, inserts it to database and returns a PurchaseTransactionId to the caller. Second method updates the PurchaseDate, gets PurchaseTransactionId and date used in update.

---
+++ SplurgeStop.Integration.Tests/HelperMethods.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SplurgeStop.Data.EF;
+using SplurgeStop.Data.EF.Repositories;
+using SplurgeStop.Domain.PurchaseTransaction;
+using SplurgeStop.UI.WebApi.Controllers;
+using transaction = SplurgeStop.Domain.PurchaseTransaction;
+
+namespace SplurgeStop.Integration.Tests
+{
+    public static class HelperMethods
+    {
+        public async static Task<PurchaseTransactionId> CreateValidPurchaseTransaction()
+        {
+            var connectionString = ConnectivityService.GetConnectionString("TEMP");
+            var context = new SplurgeStopDbContext(connectionString);
+            var repository = new PurchaseTransactionRepository(context);
+            var unitOfWork = new EfCoreUnitOfWork(context);
+            var service = new PurchaseTransactionService(repository, unitOfWork);
+            var transaction = new PurchaseTransaction();
+
+            var command = new Commands.Create();
+            command.Transaction = transaction;
+
+            var transactionController = new PurchaseTransactionController(service);
+            var result = await transactionController.Post(command);
+
+            var updateCommand = new Commands.SetPurchaseTransactionDate();
+            updateCommand.Id = transaction.Id;
+            updateCommand.TransactionDate = DateTime.Now;
+            await transactionController.Put(updateCommand);
+
+            // TODO: Update Store
+            // TODO: Update Line Items
+            return command.Transaction.Id;
+        }
+
+        public async static Task UpdatePurchaseDate(PurchaseTransactionId id, DateTime date)
+        {
+            var connectionString = ConnectivityService.GetConnectionString("TEMP");
+            var context = new SplurgeStopDbContext(connectionString);
+            var repository = new PurchaseTransactionRepository(context);
+            var unitOfWork = new EfCoreUnitOfWork(context);
+            var service = new PurchaseTransactionService(repository, unitOfWork);
+            var transactionController = new PurchaseTransactionController(service);
+
+            var updateCommand = new Commands.SetPurchaseTransactionDate();
+            updateCommand.Id = id;
+            updateCommand.TransactionDate = date;
+            await transactionController.Put(updateCommand);
+        }
+    }
+}
