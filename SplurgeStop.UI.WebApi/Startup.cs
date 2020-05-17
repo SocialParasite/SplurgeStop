85e748df5a6cebff27eddf33f89934f65a193056
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-05-17 11:34:04 +0300

    ASP.NET conf

--- SplurgeStop.UI.WebApi/Startup.cs
+++ SplurgeStop.UI.WebApi/Startup.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SplurgeStop.Data.EF;
+using SplurgeStop.Data.EF.Repositories;
+using SplurgeStop.Domain;
+using SplurgeStop.Domain.DA_Interfaces;
+using SplurgeStop.Domain.PurchaseTransaction;
 
 namespace SplurgeStop.UI.WebApi
 {
@@ -18,12 +24,29 @@ namespace SplurgeStop.UI.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connString = Configuration.GetConnectionString("DEV");
+
+            services.AddDbContext<SplurgeStopDbContext>(options => options.UseSqlServer(connString));
+
+            services.AddTransient<IUnitOfWork, EfCoreUnitOfWork>();
+            services.AddTransient<IPurchaseTransactionRepository, PurchaseTransactionRepository>();
+            services.AddTransient<PurchaseTransactionService>();
+
+            services.AddCors(options => options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyMethod()
+                                  .AllowAnyHeader()
+                                  .WithOrigins("http://localhost:3000")
+                                  .AllowCredentials()));
+
             services.AddControllers();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
