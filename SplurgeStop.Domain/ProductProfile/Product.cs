812d90af5e28c593e538a7dbb01259f03b8ed16f
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-05-22 19:32:22 +0300

    Product and ProductId added

---
+++ SplurgeStop.Domain/ProductProfile/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text;
+
+namespace SplurgeStop.Domain.ProductProfile
+{
+    public sealed class Product
+    {
+        public ProductId Id { get; set; }
+
+        //public Brand Brand { get; set; }
+        public string Name { get; set; } //ProductName
+        //public GeneralName GeneralName { get; set; }
+
+        //public Size Size { get; set; }
+
+    }
+}
---
+++ SplurgeStop.Domain/ProductProfile/ProductId.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using GuidHelpers;
+
+namespace SplurgeStop.Domain.ProductProfile
+{
+    public class ProductId : ValueObject
+    {
+        public Guid Value { get; protected set; }
+
+        internal ProductId(Guid id)
+        {
+            if (id == default)
+                throw new ArgumentException("Invalid id!", nameof(id));
+
+            Value = id;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public static implicit operator ProductId(Guid value)
+            => new ProductId(new SequentialGuid(value));
+    }
+}
