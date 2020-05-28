2cf9b51595ce9bee21d6a4f964541b1d422e7375
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-05-28 14:36:45 +0300

    StoreRepository + interface added

---
+++ SplurgeStop.Data.EF/Repositories/StoreRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SplurgeStop.Domain.DA_Interfaces;
+using SplurgeStop.Domain.StoreProfile;
+
+namespace SplurgeStop.Data.EF.Repositories
+{
+    public sealed class StoreRepository : IStoreRepository
+    {
+        private readonly SplurgeStopDbContext context;
+
+        public StoreRepository(SplurgeStopDbContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task AddStoreAsync(Store store)
+        {
+            await context.Stores.AddAsync(store);
+        }
+
+        public async Task<bool> ExistsAsync(StoreId id)
+        {
+            return await context.Stores.FindAsync(id) != null;
+        }
+
+        public async Task<Store> GetStoreFullAsync(StoreId id)
+        {
+            return await context.Stores
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<IEnumerable<Store>> GetAllStoresAsync()
+        {
+            return await context.Stores
+                    .AsNoTracking()
+                    .ToListAsync();
+        }
+
+        public async Task<Store> LoadStoreAsync(StoreId id)
+        {
+            return await context.Stores.FindAsync(id);
+        }
+
+        public async Task<Store> LoadFullStoreAsync(StoreId id)
+        {
+            return await context.Stores.FirstOrDefaultAsync(s => s.Id == id);
+        }
+    }
+}
---
+++ SplurgeStop.Domain/DA Interfaces/IStoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SplurgeStop.Domain.StoreProfile;
+
+namespace SplurgeStop.Domain.DA_Interfaces
+{
+    public interface IStoreRepository
+    {
+        Task AddStoreAsync(Store store);
+        Task<bool> ExistsAsync(StoreId id);
+        Task<IEnumerable<Store>> GetAllStoresAsync();
+        Task<Store> GetStoreFullAsync(StoreId id);
+        Task<Store> LoadFullStoreAsync(StoreId id);
+        Task<Store> LoadStoreAsync(StoreId id);
+    }
+}
