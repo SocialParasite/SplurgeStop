69272a2934682208ffa5e116eb21ba7bcaf94c53
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-05-28 14:39:45 +0300

    StoreController added

---
+++ SplurgeStop.UI.WebApi/Controllers/StoreController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using SplurgeStop.Domain.StoreProfile;
+using SplurgeStop.UI.WebApi.Common;
+
+namespace SplurgeStop.UI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IStoreService service;
+
+        public StoreController(IStoreService service)
+        {
+            this.service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Post(Commands.Create request)
+        {
+            try
+            {
+                return RequestHandler.HandleCommand(request, service.Handle);
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e, "Somehow something happened while creating a new Store =-O");
+
+                throw;
+            }
+        }
+
+        [Route("storeName")]
+        [HttpPut]
+        public Task<IActionResult> Put(Commands.SetStoreName request) 
+            => RequestHandler.HandleCommand(request, service.Handle);
+    }
+}
