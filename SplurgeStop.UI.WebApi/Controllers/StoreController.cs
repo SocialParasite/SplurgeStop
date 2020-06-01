2c51d92635f58d01ba79a9714ae29983a36f7ded
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-06-01 10:49:50 +0300

    clean up

--- SplurgeStop.UI.WebApi/Controllers/StoreController.cs
+++ SplurgeStop.UI.WebApi/Controllers/StoreController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using GuidHelpers;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
 using SplurgeStop.Domain.StoreProfile;
