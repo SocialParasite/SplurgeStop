dc20bb73b5cb79ad7665de31097c372ed83aa9cc
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-06-30 17:19:07 +0300

    Currency fix
    message and prop order switched

--- SplurgeStop.Domain/PurchaseTransaction/Currency.cs
+++ SplurgeStop.Domain/PurchaseTransaction/Currency.cs
@@ -4,14 +4,14 @@ namespace SplurgeStop.Domain.PurchaseTransaction
 {
     public class Currency
     {
-        public string CurrencyCode { get; set; }
-        public string CurrencySymbol { get; set; }
-        public CurrencySymbolPosition PositionRelativeToPrice { get; set; }
+        public string CurrencyCode { get; }
+        public string CurrencySymbol { get; }
+        public CurrencySymbolPosition PositionRelativeToPrice { get; }
 
         public Currency(string currencyCode, string currencySymbol, CurrencySymbolPosition position)
         {
             if (currencyCode == null)
-                throw new ArgumentNullException("Invalid Currency code!", nameof(currencyCode));
+                throw new ArgumentNullException(nameof(currencyCode), "Invalid Currency code!");
 
             if (currencyCode.Length != 3)
                 throw new ArgumentException("Invalid Currency code! Currency code should be three characters long.", nameof(currencyCode));
