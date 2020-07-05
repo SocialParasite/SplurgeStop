c136cc2f30baeaea9edf0e3b6bb035d7e7584369
Toni Jaakkola <toni.jaakkola@gmail.com>
2020-07-05 14:21:12 +0300

    LineItem test
    New test written (product)

--- SplurgeStop.Domain.Tests/LineItemTests.cs
+++ SplurgeStop.Domain.Tests/LineItemTests.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using GuidHelpers;
+using SplurgeStop.Domain.ProductProfile;
+using SplurgeStop.Domain.ProductProfile.BrandProfile;
 using SplurgeStop.Domain.PurchaseTransaction;
 using SplurgeStop.Domain.PurchaseTransaction.PriceProfile;
 using Xunit;
+using Price = SplurgeStop.Domain.PurchaseTransaction.PriceProfile.Price;
 
 namespace SplurgeStop.Domain.Tests
 {
@@ -25,6 +29,19 @@ namespace SplurgeStop.Domain.Tests
             var sut = LineItemBuilder.LineItem(price).WithNotes("No kun sai niin halvalla.").Build();
 
             Assert.Contains("halvalla", sut.Notes);
+        }
+
+        [Fact]
+        public void Has_product()
+        {
+            var price = new Price(1.00m, Booking.Credit, "EUR", "€", CurrencySymbolPosition.End);
+
+            var brand = new Brand();
+            var prodId = new ProductId(SequentialGuid.NewSequentialGuid());
+            var product = Product.Create(prodId, "newProd", brand);
+            var sut = LineItemBuilder.LineItem(price).WithProduct(product).Build();
+
+            Assert.Contains("newProd", sut.Product.Name);
         }
 
         [Fact]
